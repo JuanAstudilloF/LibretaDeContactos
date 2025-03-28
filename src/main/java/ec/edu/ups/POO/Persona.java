45e647212612a786e3eaac3ca99a36f888936f4a
Juan Astudillo F <juanandresastudillo9@gmail.com>
2025-03-28 12:37:46 -0500

    Inicial Commit

--- src/main/java/ec/edu/ups/POO/Persona.java
+++ src/main/java/ec/edu/ups/POO/Persona.java
@@ -18,32 +18,16 @@ public class Persona {
         this.direccion = direccion;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    public String getCedula() {
-        return cedula;
     }
 
     public void setCedula(String cedula) {
         this.cedula = cedula;
     }
 
-    public String getApellido() {
-        return apellido;
-    }
-
     public void setApellido(String apellido) {
         this.apellido = apellido;
-    }
-
-    public String getDireccion() {
-        return direccion;
     }
 
     public void setDireccion(String direccion) {
