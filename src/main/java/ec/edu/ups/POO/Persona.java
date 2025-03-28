2477bb9b13bd404d3a975e7863a6c46f4d66d6f8
Juan Astudillo F <juanandresastudillo9@gmail.com>
2025-03-28 12:41:01 -0500

    Inicial Commit

--- src/main/java/ec/edu/ups/POO/Persona.java
+++ src/main/java/ec/edu/ups/POO/Persona.java
@@ -34,9 +34,6 @@ public class Persona {
         this.direccion = direccion;
     }
 
-    /**
-     * @return
-     */
     @Override
     public String toString(){
         return "Cedula" + cedula + "\nNombre: " + nombre + "\nApellido: " + apellido + "\nDireccion: " + direccion;
