e640f0ee79de221a5b146dc09a395c19fcea2e52
Juan Astudillo F <juanandresastudillo9@gmail.com>
2025-03-28 12:35:13 -0500

    Inicial Commit

--- src/main/java/ec/edu/ups/POO/Persona.java
+++ src/main/java/ec/edu/ups/POO/Persona.java
@@ -2,50 +2,58 @@ package ec.edu.ups.POO;
 
 public class Persona {
 
-    private String cedula;
     private String nombre;
     private String apellido;
+    private String cedula;
     private String direccion;
 
-    public Persona(String cedula){
-        this.cedula = cedula;
+
+    public Persona() {
     }
 
-    public Persona(){
-
-    }
-
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-
-    public void setNombre(String nombre) {
+    public Persona(String nombre, String apellido, String cedula, String direccion) {
         this.nombre = nombre;
-    }
-
-    public void setApellido(String apellido) {
         this.apellido = apellido;
-    }
-
-    public void setDireccion(String direccion) {
+        this.cedula = cedula;
         this.direccion = direccion;
-    }
-
-    // Getters
-    public String getCedula() {
-        return cedula;
     }
 
     public String getNombre() {
         return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
     public String getApellido() {
         return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
 
     public String getDireccion() {
         return direccion;
     }
-}
 
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString(){
+        return "Cedula" + cedula + "\nNombre: " + nombre + "\nApellido: " + apellido + "\nDireccion: " + direccion;
+
+    }
+
+}
--- src/main/java/ec/edu/ups/POO/Principal.java
+++ src/main/java/ec/edu/ups/POO/Principal.java
@@ -1,119 +1,96 @@
 package ec.edu.ups.POO;
+
+import ec.edu.ups.poo.clases.Familiar;
+import ec.edu.ups.poo.clases.Persona;
+
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Persona p1 = new Persona("0101010");
-        Persona p2 = new Persona();
-        Persona p3 = new Persona();
-        Persona p4 = new Persona();
-        Persona p5 = new Persona();
-        Persona p6 = new Persona();
-        Persona p7 = new Persona();
-        Persona p8 = new Persona();
-        Persona p9 = new Persona();
-        Persona p10 = new Persona();
+        System.out.println("Ingrese cuantas personas desea registrar: ");
+        int nPersonas = sc.nextInt();
+        sc.nextLine();
 
-        System.out.println("Ingrese datos de la Persona 1:");
-        System.out.print("Cedula: ");
-        p1.setCedula(sc.nextLine());
-        System.out.print("Nombre: ");
-        p1.setNombre(sc.nextLine());
-        System.out.print("Apellido: ");
-        p1.setApellido(sc.nextLine());
-        System.out.print("Direccion: ");
-        p1.setDireccion(sc.nextLine());
+        Persona[] personas = new Persona[nPersonas];
 
-        System.out.println("\nIngrese datos de la Persona 2:");
-        p2.setCedula(sc.nextLine());
-        p2.setNombre(sc.nextLine());
-        p2.setApellido(sc.nextLine());
-        p2.setDireccion(sc.nextLine());
+        for (int i = 0; i < nPersonas; i++) {
+            Persona persona = new Persona();
 
-        System.out.println("\nIngrese datos de la Persona 3:");
-        p3.setCedula(sc.nextLine());
-        p3.setNombre(sc.nextLine());
-        p3.setApellido(sc.nextLine());
-        p3.setDireccion(sc.nextLine());
+            System.out.println("\n--- Ingrese los datos de la persona " + (i + 1) + " ---");
+            System.out.print("Nombre: ");
+            persona.setNombre(sc.nextLine());
 
-        System.out.println("\nIngrese datos de la Persona 4:");
-        p4.setCedula(sc.nextLine());
-        p4.setNombre(sc.nextLine());
-        p4.setApellido(sc.nextLine());
-        p4.setDireccion(sc.nextLine());
+            System.out.print("Apellido: ");
+            persona.setApellido(sc.nextLine());
 
-        System.out.println("\nIngrese datos de la Persona 5:");
-        p5.setCedula(sc.nextLine());
-        p5.setNombre(sc.nextLine());
-        p5.setApellido(sc.nextLine());
-        p5.setDireccion(sc.nextLine());
+            System.out.print("Cedula: ");
+            persona.setCedula(sc.nextLine());
 
-        System.out.println("\nIngrese datos de la Persona 6:");
-        p6.setCedula(sc.nextLine());
-        p6.setNombre(sc.nextLine());
-        p6.setApellido(sc.nextLine());
-        p6.setDireccion(sc.nextLine());
+            System.out.print("Direccion: ");
+            persona.setDireccion(sc.nextLine());
 
-        System.out.println("\nIngrese datos de la Persona 7:");
-        p7.setCedula(sc.nextLine());
-        p7.setNombre(sc.nextLine());
-        p7.setApellido(sc.nextLine());
-        p7.setDireccion(sc.nextLine());
+            personas[i] = persona;
+        }
 
-        System.out.println("\nIngrese datos de la Persona 8:");
-        p8.setCedula(sc.nextLine());
-        p8.setNombre(sc.nextLine());
-        p8.setApellido(sc.nextLine());
-        p8.setDireccion(sc.nextLine());
+        System.out.println("\n¿Cuantos familiares desea registrar?");
+        int nFamiliares = sc.nextInt();
+        sc.nextLine();
 
-        System.out.println("\nIngrese datos de la Persona 9:");
-        p9.setCedula(sc.nextLine());
-        p9.setNombre(sc.nextLine());
-        p9.setApellido(sc.nextLine());
-        p9.setDireccion(sc.nextLine());
+        Familiar[] familiares = new Familiar[nFamiliares];
 
-        System.out.println("\nIngrese datos de la Persona 10:");
-        p10.setCedula(sc.nextLine());
-        p10.setNombre(sc.nextLine());
-        p10.setApellido(sc.nextLine());
-        p10.setDireccion(sc.nextLine());
+        for (int i = 0; i < nFamiliares; i++) {
+            Familiar familiar = new Familiar();
 
-        System.out.println("\n--- Datos de las Personas ---");
-        System.out.println("Persona 1: Cedula: " + p1.getCedula() + ", Nombre: " + p1.getNombre() + " " + p1.getApellido() + ", Direccion: " + p1.getDireccion());
-        System.out.println("Persona 2: Cedula: " + p2.getCedula() + ", Nombre: " + p2.getNombre() + " " + p2.getApellido() + ", Direccion: " + p2.getDireccion());
-        System.out.println("Persona 3: Cedula: " + p3.getCedula() + ", Nombre: " + p3.getNombre() + " " + p3.getApellido() + ", Direccion: " + p3.getDireccion());
-        System.out.println("Persona 4: Cedula: " + p4.getCedula() + ", Nombre: " + p4.getNombre() + " " + p4.getApellido() + ", Direccion: " + p4.getDireccion());
-        System.out.println("Persona 5: Cedula: " + p5.getCedula() + ", Nombre: " + p5.getNombre() + " " + p5.getApellido() + ", Direccion: " + p5.getDireccion());
-        System.out.println("Persona 6: Cedula: " + p6.getCedula() + ", Nombre: " + p6.getNombre() + " " + p6.getApellido() + ", Direccion: " + p6.getDireccion());
-        System.out.println("Persona 7: Cedula: " + p7.getCedula() + ", Nombre: " + p7.getNombre() + " " + p7.getApellido() + ", Direccion: " + p7.getDireccion());
-        System.out.println("Persona 8: Cedula: " + p8.getCedula() + ", Nombre: " + p8.getNombre() + " " + p8.getApellido() + ", Direccion: " + p8.getDireccion());
-        System.out.println("Persona 9: Cedula: " + p9.getCedula() + ", Nombre: " + p9.getNombre() + " " + p9.getApellido() + ", Direccion: " + p9.getDireccion());
-        System.out.println("Persona 10: Cedula: " + p10.getCedula() + ", Nombre: " + p10.getNombre() + " " + p10.getApellido() + ", Direccion: " + p10.getDireccion());
+            System.out.println("\n--- Ingrese los datos del familiar " + (i + 1) + " ---");
+            System.out.print("Nombre: ");
+            familiar.setNombre(sc.nextLine());
 
-        sc.close();
+            System.out.print("Apellido: ");
+            familiar.setApellido(sc.nextLine());
+
+            System.out.print("Cedula: ");
+            familiar.setCedula(sc.nextLine());
+
+            System.out.print("Direccion: ");
+            familiar.setDireccion(sc.nextLine());
+
+            System.out.print("Parentesco: ");
+            familiar.setParentesco(sc.nextLine());
+
+            System.out.print("Tipo de sangre: ");
+            familiar.setTipoDeSangre(sc.nextLine());
+
+            System.out.print("Año de nacimiento: ");
+            int anioNacimiento = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Mes de nacimiento: ");
+            int mesNacimiento = sc.nextInt() - 1;
+            sc.nextLine();
+
+            System.out.print("Dia de nacimiento: ");
+            int diaNacimiento = sc.nextInt();
+            sc.nextLine();
+
+            GregorianCalendar fechaNacimiento = new GregorianCalendar(anioNacimiento, mesNacimiento, diaNacimiento);
+            familiar.setFechaNacimiento(fechaNacimiento);
+
+            familiares[i] = familiar;
+        }
+
+        System.out.println("\n--- Lista de Personas ---");
+        for (Persona persona : personas) {
+            System.out.println(persona);
+            System.out.println();
+        }
+
+        System.out.println("\n--- Lista de Familiares ---");
+        for (Familiar familiar : familiares) {
+            System.out.println(familiar);
+            System.out.println();
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
